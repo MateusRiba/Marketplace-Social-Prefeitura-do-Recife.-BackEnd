3073b54a472307cf271db3d1e582e47508221e8c
MateusRiba <Mateusbaaca@gmail.com>
2025-04-04 09:34:36 -0300

    Update products.js

--- src/routes/products.js
+++ src/routes/products.js
@@ -67,7 +67,7 @@ router.get('/size/:size', ProductController.getProductsBySize);
  * Remove um produto pelo seu ID
  * Apenas Admin
  */
-router.delete('/:id', requireAdmin, ProductController.removeProduct);
+router.delete('/:id', ProductController.removeProduct);
 
 
 /* Atualização de produto(PUT /praducts/:id)
